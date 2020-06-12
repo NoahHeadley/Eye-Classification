868f020877f7d9e29f264e8eedda8f3968052cae
NoahHeadley <noahkito@gmail.com>
2020-06-12 10:28:02 -0400

    Spark Parallel Computing used
    The parallel computing cuts the runtime for the program by over half the previous time but requires much more processing power

--- clusterize.py
+++ clusterize.py
@@ -9,6 +9,7 @@ import argparse
 import os
 import math
 from datetime import datetime
+from pyspark.sql import SparkSession
 
 
 def build_arg_parser():
@@ -23,15 +24,17 @@ def get_dist(x1, y1, x2, y2):
     return dist
 
 
-def get_eye_info(right_eye, left_eye, image):
+def get_eye_info(face):
     """
     Calculates all the wanted variables for the pictures of eyes
 
     Parameters:
+    face: a list containing the following
     right_eye (List(int,int)): Normalized coordinates of the landmarks for the right eye
     left_eye (List(int, int)): Normalized coordinates of the landmarks for the left eye
     image (String): The filename of the image
     """
+    (right_eye, left_eye, image) = face
     area_1 = fd.find_area(right_eye)
     area_2 = fd.find_area(left_eye)
     width_1, height_1, iris_area_1, covered_1, width_2, height_2, iris_area_2, covered_2 = iris.get_iris(
@@ -40,7 +43,18 @@ def get_eye_info(right_eye, left_eye, image):
     return area_1, area_2, width_1, width_2, height_1, height_2, iris_area_1, iris_area_2, covered_1, covered_2
 
 
+def apache_face_decode(file):
+    path = 'faces/'
+    image = os.fsdecode(file)
+    face = fd.get_face_features(shape_predictor, path + image, 'eyes')
+    return (face, image)
+
+
 if __name__ == '__main__':
+    spark = SparkSession\
+        .builder\
+        .appName("Eye Data")\
+        .getOrCreate()
     args = build_arg_parser()
     shape_predictor = args["shape_predictor"]
 
@@ -54,20 +68,69 @@ if __name__ == '__main__':
     count = 1
     total = len(directory)
     start = datetime.now()
-    for file in directory:
-        image = os.fsdecode(file)
-        face = fd.get_face_features(shape_predictor, path + image, 'eyes')
-        right_eye = face[0]
-        left_eye = face[1]
-        area_1, area_2, width_1, width_2, height_1, height_2, iris_area_1, iris_area_2, covered_1, covered_2 = get_eye_info(
-            right_eye, left_eye, image)
-        dist = get_dist(right_eye[0, 0], right_eye[0, 1],
-                        left_eye[0, 0], left_eye[0, 1])
+
+    eye_variables = spark.sparkContext.parallelize(
+        directory).map(apache_face_decode)
+    eye_values = eye_variables.collect()
+
+    faces = []
+    filenames = []
+
+    for v in eye_values:
+        (face, filename) = v
+        faces.append(face)
+        filenames.append(filename)
+    # for file in directory:
+    #     image = os.fsdecode(file)
+    #     face = fd.get_face_features(shape_predictor, path + image, 'eyes')
+    #     faces.append(face)
+    #     filenames.append(image)
+    #     time_elapsed = datetime.now() - start
+    #     average_time = time_elapsed/count
+    #     expected = average_time * (total - count)
+    #     print(
+    #         f"Done: {count} / {total} = {round(float(count/total) * 100, 2)}%\tTime Elapsed:{time_elapsed}\tRemaining time:{expected}")
+    #     count += 1
+
+    eyes = []
+    for x in range(total):
+        eyes.append((faces[x][0], faces[x][1], filenames[x]))
+
+    data = spark.sparkContext.parallelize(
+        eyes).map(get_eye_info)
+
+    values = data.collect()
+
+    for v in values:
+        area_1 = v[0]
+        area_2 = v[1]
+        width_1 = v[2]
+        width_2 = v[3]
+        height_1 = v[4]
+        height_2 = v[5]
+        iris_area_1 = v[6]
+        iris_area_2 = v[7]
+        covered_1 = v[8]
+        covered_2 = v[9]
         out_file.write(
-            f"{area_1} {area_2} {width_1} {width_2} {height_1} {height_2} {iris_area_1} {iris_area_2} {covered_1} {covered_2} {dist}\n")
-        time_elapsed = datetime.now() - start
-        average_time = time_elapsed/count
-        print(
-            f"Done: {count} / {total} = {round(float(count/total) * 100, 2)}%\tTime Elapsed:{time_elapsed}\tAverage Time: {average_time}")
-        count += 1
+            f"{area_1} {area_2} {width_1} {width_2} {height_1} {height_2} {iris_area_1} {iris_area_2} {covered_1} {covered_2}\n")
+
+    # for i in range(len(directory)):
+    #     image = filenames[i]
+    #     face = faces[i]
+    #     right_eye = face[0]
+    #     left_eye = face[1]
+    #     area_1, area_2, width_1, width_2, height_1, height_2, iris_area_1, iris_area_2, covered_1, covered_2 = get_eye_info(
+    #         right_eye, left_eye, image)
+    #     dist = get_dist(right_eye[0, 0], right_eye[0, 1],
+    #                     left_eye[0, 0], left_eye[0, 1])
+    #     out_file.write(
+    #         f"{area_1} {area_2} {width_1} {width_2} {height_1} {height_2} {iris_area_1} {iris_area_2} {covered_1} {covered_2} {dist}\n")
+    #     time_elapsed = datetime.now() - start
+    #     average_time = time_elapsed/count
+    #     print(
+    #         f"Done: {count} / {total} = {round(float(count/total) * 100, 2)}%\tTime Elapsed:{time_elapsed}\tAverage Time: {average_time}")
+    #     count += 1
+
     out_file.close()
+    spark.stop()
